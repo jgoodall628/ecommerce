e1be7797969187967d316be4023177c76fd073ad
Jeffrey Goodall <jgoodall628@gmail.com>
2015-09-15 09:21:15 -0500

    search by brand added initial commit before adding search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+
 
   before_filter :brands
 
   def brands
-    @products = Product.all
-    @brands = []
-
-    @products.each do |product|
-      unless @brands.include?(product.brand)
-        @brands.push(product.brand)
-      end
-    end
+    @brands = Product.uniq.pluck(:brand)
   end
 end
--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -6,19 +6,11 @@ class StorefrontController < ApplicationController
   def items_by_category
     @category = Category.find(params[:cat_id])
     @products = @category.products
-    # @products = Product.all
-    # @products_by_cat = []
-    #
-    # @products.each do |product|
-    #   if product.category_id == params[:cat_id].to_i
-    #     @products_by_cat.push(product)
-    #   end
-    # end
   end
 
   def items_by_brand
     @brand = params[:brand]
-    @products = @products.where(brand: @brand)
+    @products = Product.where(brand: @brand)
 
   end
 end
